594a9056ecf7e0d65ed1bb24c557cb96bd9340de
lebowski89 <nosugarmaxtaste@gmail.com>
2026-04-26 07:04:30 +0000

    uptime-kuma module overhaul + add postgres + public service monitoring

--- terraform/uptime-kuma/monitors-postgres.tf
+++ terraform/uptime-kuma/monitors-postgres.tf
@@ -1,19 +1,23 @@
-resource "uptimekuma_monitor_ping" "this" {
-  for_each = local.ping_monitors
+resource "uptimekuma_monitor_postgres" "this" {
+  for_each = local.postgres_monitors
 
-  name            = each.value.name
-  hostname        = each.value.hostname
-  description     = each.value.description
+  name                       = each.value.name
+  database_connection_string = var.postgres_monitor_connection_strings[each.key]
+  database_query             = "SELECT 1"
+  description                = each.value.description
+
   interval        = local.default_monitor.interval
   max_retries     = local.default_monitor.max_retries
   retry_interval  = local.default_monitor.retry_interval
   resend_interval = local.default_monitor.resend_interval
   active          = local.default_monitor.active
   upside_down     = local.default_monitor.upside_down
+
   parent = coalesce(
     try(uptimekuma_monitor_group.root[each.value.group].id, null),
     try(uptimekuma_monitor_group.child[each.value.group].id, null)
   )
+
   notification_ids = local.default_notification_ids
 
   tags = [
--- terraform/uptime-kuma/tags.tf
+++ terraform/uptime-kuma/tags.tf
@@ -1,23 +1,22 @@
 locals {
   tags = {
-    arrs           = { name = "arrs",           color = "#7c3aed" }
-    auth           = { name = "auth",           color = "#7c3aed" }
-    core           = { name = "core",           color = "#475569" }
-    critical       = { name = "critical",       color = "#dc2626" }
-    dns            = { name = "dns",            color = "#9333ea" }
+    arrs           = { name = "arrs", color = "#7c3aed" }
+    auth           = { name = "auth", color = "#7c3aed" }
+    critical       = { name = "critical", color = "#dc2626" }
+    dns            = { name = "dns", color = "#9333ea" }
     infrastructure = { name = "infrastructure", color = "#2563eb" }
-    private        = { name = "private",        color = "#64748b" }
-    public         = { name = "public",         color = "#0284c7" }
-    media          = { name = "media",          color = "#22c55e" }
-    monitoring     = { name = "monitoring",     color = "#16a34a" }
-    networking     = { name = "networking",     color = "#0ea5e9" }
-    notifications  = { name = "notifications",  color = "#eab308" }
-    plex           = { name = "plex",           color = "#d97706" }
-    storage        = { name = "storage",        color = "#a855f7" }
-    torrents       = { name = "torrents",       color = "#ca8a04" }
-    traefik        = { name = "traefik",        color = "#f97316" }
-    usenet         = { name = "usenet",         color = "#0284c7" }
-    utilities      = { name = "utilities",      color = "#4b5563" }
+    private        = { name = "private", color = "#64748b" }
+    public         = { name = "public", color = "#0284c7" }
+    media          = { name = "media", color = "#22c55e" }
+    monitoring     = { name = "monitoring", color = "#16a34a" }
+    network        = { name = "network", color = "#0ea5e9" }
+    networking     = { name = "networking", color = "#0ea5e9" }
+    plex           = { name = "plex", color = "#d97706" }
+    storage        = { name = "storage", color = "#a855f7" }
+    torrents       = { name = "torrents", color = "#ca8a04" }
+    traefik        = { name = "traefik", color = "#f97316" }
+    usenet         = { name = "usenet", color = "#0284c7" }
+    utilities      = { name = "utilities", color = "#4b5563" }
   }
 }
 
