4d260b5c9effe0ccc7cf8b32d98574784da2e3ff
lebowski89 <nosugarmaxtaste@gmail.com>
2026-04-29 17:20:51 +0000

    add seerr tofu module

---
+++ terraform/seerr/variables.tf
@@ -0,0 +1,42 @@
+variable "seerr_url" {
+  type = string
+}
+
+variable "seerr_api_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "domain_int" {
+  type = string
+}
+
+variable "tautulli_api_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "gotify_token" {
+  type      = string
+  sensitive = true
+}
+
+variable "radarr_api_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "radarr_4k_api_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "sonarr_api_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "sonarr_4k_api_key" {
+  type      = string
+  sensitive = true
+}
