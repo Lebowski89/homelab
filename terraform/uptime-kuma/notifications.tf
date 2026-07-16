9e7dc072dbf6e70593fb684cfa4119110d6f1110
lebowski89 <nosugarmaxtaste@gmail.com>
2026-07-16 12:11:04 +0000

    fix(uptime-kuma): source topology inputs from NetBox

--- terraform/uptime-kuma/notifications.tf
+++ terraform/uptime-kuma/notifications.tf
@@ -2,7 +2,7 @@ resource "uptimekuma_notification_gotify" "gotify" {
   count = var.enable_gotify_notification ? 1 : 0
 
   name              = "Gotify"
-  server_url        = var.gotify_server_url
+  server_url        = local.gotify_server_url
   application_token = var.gotify_application_token
   priority          = var.gotify_priority
   is_active         = true
